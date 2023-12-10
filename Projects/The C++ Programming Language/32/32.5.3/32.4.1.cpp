1d27621b85869e33155ede6901252b3c541a743a
yonis3 <143237128+yonis3@users.noreply.github.com>
2023-12-09 19:16:04 -0500

    Update 32.4.1.cpp

--- Projects/The C++ Programming Language/32/32.5.3/32.4.1.cpp
+++ Projects/The C++ Programming Language/32/32.5.3/32.4.1.cpp
@@ -2,24 +2,49 @@
 #include <vector>
 #include <algorithm>
 
-void increment_all(std::vector<int>& v) {
-    // Increment each element of the vector using for_each
-    std::for_each(v.begin(), v.end(), [](int& x) { ++x; });
+template<typename T>
+void printContainer(const T& container) {
+    for (const auto& element : container) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main() {
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    // Initialize a vector
+    std::vector<int> numbers = {1, 2, 3, 4, 2, 5, 2, 6};
 
-    // Use for_each to print each element
-    std::for_each(numbers.begin(), numbers.end(), [](int x) { std::cout << x << " "; });
-    std::cout << std::endl;
+    // Remove elements with value 2 using remove
+    auto removeEnd = std::remove(numbers.begin(), numbers.end(), 2);
+    printContainer(numbers);
 
-    // Use the increment_all function to increment each element
-    increment_all(numbers);
+    // Remove elements satisfying a condition using remove_if
+    auto removeIfEnd = std::remove_if(numbers.begin(), numbers.end(),
+                                      [](int x) { return x % 2 == 0; });
+    printContainer(numbers);
 
-    // Print the incremented elements
-    std::for_each(numbers.begin(), numbers.end(), [](int x) { std::cout << x << " "; });
-    std::cout << std::endl;
+    // Remove and copy elements with value 2 using remove_copy
+    std::vector<int> removedCopy(numbers.size());
+    auto removeCopyEnd = std::remove_copy(numbers.begin(), numbers.end(),
+                                          removedCopy.begin(), 2);
+    printContainer(removedCopy);
+
+    // Remove and copy elements satisfying a condition using remove_copy_if
+    std::vector<int> removedCopyIf(numbers.size());
+    auto removeCopyIfEnd = std::remove_copy_if(numbers.begin(), numbers.end(),
+                                               removedCopyIf.begin(),
+                                               [](int x) { return x % 2 == 0; });
+    printContainer(removedCopyIf);
+
+    // Reverse the order of elements using reverse
+    std::reverse(numbers.begin(), numbers.end());
+    printContainer(numbers);
+
+    // Copy and reverse the order of elements using reverse_copy
+    std::vector<int> reverseCopy(numbers.size());
+    auto reverseCopyEnd = std::reverse_copy(numbers.begin(), numbers.end(),
+                                            reverseCopy.begin());
+    printContainer(reverseCopy);
 
     return 0;
 }
